277ed1d184ae5a22a9efb928b290306846ea3678
Specticall <josephyusmita@gmail.com>
2025-03-10 19:24:04 +0700

    pertemuan 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,5 @@
-import { useState } from "react";
-import Checkout from "./Checkout";
-import Form from "./Form";
+import SuccessCheckout from "./SuccessPayment";
 
 export default function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [method, setMethod] = useState("e-money");
-
-  console.log({ name, email, phone, method });
-
-  return (
-    <div className="container">
-      <Form
-        name={name}
-        phone={phone}
-        email={email}
-        method={method}
-        setName={setName}
-        setPhone={setPhone}
-        setEmail={setEmail}
-        setMethod={setMethod}
-      />
-      <Checkout />
-    </div>
-  );
+  return <SuccessCheckout />;
 }
